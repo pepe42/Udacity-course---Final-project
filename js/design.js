85b2fe2a0360b897529216a8367176acb931c1b2
pepe <designx.click@gmail.com>
2018-01-27 22:45:18 +0100

    New js style

--- js/design.js
+++ js/design.js
@@ -1,83 +1,108 @@
-// H1 TITLE INTRO
-$('h1').hide().fadeIn(3000);
 
-//SOCIAL-ICONS ANIMATION
-var icons = $('.social-icon'),
-	styles = {
-		position: 'relative',
-		color: 'black',
-		zIndex: 10
-	},
-	settings = { opacity: .5 },
-	speed = 5000;
-
-icons.css(styles);
-
-icons.on('mouseenter', function() {
-	$(this).fadeOut(settings, speed).fadeIn(settings, speed);
-})
-
-// RESPONSIVE MENU
-$('.contact-respo').hide();
-
-$('#menu').on('click', function() {
-		$('.contact-respo').toggle();
-	});
-
-// Udacity logo IMAGE ROTATION
-$(".logo").rotate({
-  bind:
-  {
-    mouseover : function() {
-    $(this).rotate({animateTo:360})
-  },
-  mouseout : function() {
-    $(this).rotate({animateTo:0})
-    }
-  }
+$(document).ready(function () {
+  	setTitle();
+  	setSocialIcons();
+  	setMenu();
+  	setLogo();
+  	setGrid();
 });
 
+function setTitle()
+{
+	// H1 TITLE INTRO
+	$('h1').hide().fadeIn(3000);
+}
+
+function setSocialIcons()
+{
+	//SOCIAL-ICONS ANIMATION
+	var icons = $('.social-icon'),
+		styles = {
+			position: 'relative',
+			color: 'black',
+			zIndex: 10
+		},
+		settings = { opacity: .5 },
+		speed = 5000;
+
+	icons.css(styles);
+
+	icons.on('mouseenter', function() {
+		$(this).fadeOut(settings, speed).fadeIn(settings, speed);
+	})
+}
 
 
+function setMenu()
+{
+	// RESPONSIVE MENU
+	$('.contact-respo').hide();
+
+	$('#menu').on('click', function() {
+			$('.contact-respo').toggle();
+		});
+}
 
 
+function setLogo()
+{
+	// Udacity logo IMAGE ROTATION
+	$(".logo").rotate({
+	  bind:
+	  {
+	    mouseover : function() {
+	    $(this).rotate({animateTo:360})
+	  },
+	  mouseout : function() {
+	    $(this).rotate({animateTo:0})
+	    }
+	  }
+	});
+}
 
 
-
-
-
-
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-/*function makeGrid() {
-
-// Your code goes here!
-
-}*/
-
-
-//Benjamin code
-$(document).ready(function () {
-  	$('#sizePicker').submit(function makeGrid(grid) {
+function setGrid()
+{
+	$('#sizePicker').submit(function makeGrid(grid) {
+		//remove all tables
     	$('table tr').remove();
-	    var theRow = $('#input_height').val();
-	    var theCol = $('#input_width').val();
-		    for (var i = 1; i <= theRow; i++) {
-				$('table').append("<tr></tr>");
-		      	for (var j = 1; j <= theCol; j++) {
-		        $('tr:last').append("<td></td>");
-		        $('td').attr("class", 'cells')
-		      	}
-		    }
+	    
+	    var row_input = $('#sizePicker #input_height').val();
+	    var col_input = $('#input_width').val();
+	    
+	    for (var i = 1; i <= row_input; i++) 
+	    {
+			createRow();
+	      	for (var j = 1; j <= col_input; j++) 
+	      	{
+	      		createCell();
+	      	}
+	    }
+
 	    grid.preventDefault();
 
-	    $('.cells').click(function (event) {
-	    	var color = $('#colorPicker').val();
-	      	$(event.target).css('background-color', color);
-	    });
 	});
-});
+}
+
+function createRow()
+{
+	$('table').append("<tr></tr>");
+}
+function createCell()
+{
+	// append cells to last row
+    $('table tr:last').append("<td></td>");
+    // format cell
+    $('td').attr("class", 'cells');
+    // add click
+    $('td').click(function (event) {
+		var paint = $('#colorPicker').val();
+	  	$(event.target).css('background-color', paint);
+	});
+}
+
+
+
+
+
 
